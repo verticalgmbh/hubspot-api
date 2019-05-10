582133ce5e53585f2cec443cdefd51c0fd92d120
Toni Gottschall <toni.gottschall@nightlycode.de>
2019-05-10 11:44:13 +0200

    fixed issues with httpclient handling

--- Vertical.HubSpot.Api/HubSpotRestClient.cs
+++ Vertical.HubSpot.Api/HubSpotRestClient.cs
@@ -14,8 +14,8 @@ namespace Vertical.HubSpot.Api {
     /// rest client used to access hubspot
     /// </summary>
     public class HubSpotRestClient {
-        readonly Uri baseaddress;
         readonly string apikey;
+        readonly HttpClient client = new HttpClient();
 
         /// <summary>
         /// creates a new <see cref="HubSpotRestClient"/>
@@ -24,7 +24,7 @@ namespace Vertical.HubSpot.Api {
         /// <param name="baseaddress">baseaddress of hubspot api</param>
         public HubSpotRestClient(string apikey, Uri baseaddress) {
             this.apikey = apikey;
-            this.baseaddress = baseaddress;
+            client.BaseAddress = baseaddress;
         }
 
         async Task CheckForError(JToken request, HttpResponseMessage response) {
@@ -65,15 +65,13 @@ namespace Vertical.HubSpot.Api {
         /// <param name="parameters">additional query parameters</param>
         /// <returns>response data</returns>
         public async Task<T> Post<T>(string url, JToken request, params Parameter[] parameters)
-            where T : JToken
-        {
+            where T : JToken {
             url += $"?hapikey={apikey}";
             if (parameters.Length > 0)
                 url += $"&{string.Join("&", parameters.Select(p => p.Key + "=" + HttpUtility.UrlEncode(p.Value)))}";
 
-            using (HttpClient client = new HttpClient()) {
-                client.BaseAddress = baseaddress;
-                HttpResponseMessage response=await client.PostAsync(url, new StringContent(request.ToString(), Encoding.UTF8, "application/json"));
+            HttpResponseMessage response = await client.PostAsync(url, new StringContent(request.ToString(), Encoding.UTF8, "application/json"));
+            using (response) {
                 await CheckForError(request, response);
                 return await ReadResponse<T>(response);
             }
@@ -87,15 +85,13 @@ namespace Vertical.HubSpot.Api {
         /// <param name="parameters">additional query parameters</param>
         /// <returns>response data</returns>
         public async Task<T> Put<T>(string url, JToken request, params Parameter[] parameters)
-            where T : JToken
-        {
+            where T : JToken {
             url += $"?hapikey={apikey}";
             if (parameters.Length > 0)
                 url += $"&{string.Join("&", parameters.Select(p => p.Key + "=" + HttpUtility.UrlEncode(p.Value)))}";
 
-            using (HttpClient client = new HttpClient()) {
-                client.BaseAddress = baseaddress;
-                HttpResponseMessage response = await client.PutAsync(url, new StringContent(request.ToString(), Encoding.UTF8, "application/json"));
+            HttpResponseMessage response = await client.PutAsync(url, new StringContent(request.ToString(), Encoding.UTF8, "application/json"));
+            using (response) {
                 await CheckForError(request, response);
                 return await ReadResponse<T>(response);
             }
@@ -107,13 +103,10 @@ namespace Vertical.HubSpot.Api {
         /// <param name="url">url to post the request to</param>
         /// <returns>response data</returns>
         public async Task<T> Delete<T>(string url)
-            where T : JToken
-        {
+            where T : JToken {
             url += $"?hapikey={apikey}";
-            using (HttpClient client = new HttpClient())
-            {
-                client.BaseAddress = baseaddress;
-                HttpResponseMessage response = await client.DeleteAsync(url);
+            HttpResponseMessage response = await client.DeleteAsync(url);
+            using (response) {
                 await CheckForError(null, response);
                 return await ReadResponse<T>(response);
             }
@@ -126,16 +119,13 @@ namespace Vertical.HubSpot.Api {
         /// <param name="parameters">additional query parameters</param>
         /// <returns>response data</returns>
         public async Task<T> Get<T>(string url, params Parameter[] parameters)
-            where T : JToken
-        {
+            where T : JToken {
             url += $"?hapikey={apikey}";
             if (parameters.Length > 0)
                 url += $"&{string.Join("&", parameters.Select(p => p.Key + "=" + HttpUtility.UrlEncode(p.Value)))}";
 
-            using (HttpClient client = new HttpClient())
-            {
-                client.BaseAddress = baseaddress;
-                HttpResponseMessage response = await client.GetAsync(url);
+            HttpResponseMessage response = await client.GetAsync(url);
+            using (response) {
                 await CheckForError(null, response);
                 return await ReadResponse<T>(response);
             }

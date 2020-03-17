e585b5b716e343b08ddbe7e9f918767aaa294e13
Frank Løvendahl Nielsen <fln@microknights.dk>
2020-03-17 16:18:27 +0100

    enabled support for JsonProperty and JsonIgnore in Entity model types, and a test

---
+++ Vertical.HubSpot.Test/Model/TestModelRegistry.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
+using Vertical.HubSpot.Api.Data;
+using Vertical.HubSpot.Api.Models;
+using Xunit;
+
+namespace Vertical.HubSpot.Test.Model
+{
+    public class TestModelRegistry
+    {
+        private class TestJsonProperties
+        {
+            [JsonProperty("name2")]
+            public string Name1 { get; set; }
+
+            public string NameA { get; set; }
+
+            [JsonIgnore]
+            public string Ignore { get; set; }
+        }
+
+        private class TestInternalProperties
+        {
+            [Name("name2")]
+            public string Name1 { get; set; }
+
+            public string NameA { get; set; }
+
+            [IgnoreDataMember]
+            public string Ignore { get; set; }
+        }
+
+
+        [Theory]
+        [InlineData(typeof(TestInternalProperties))]
+        [InlineData(typeof(TestJsonProperties))]
+        public void TestNamedAndIgnoredProerties(Type type)
+        {
+            var modelRegistry = new ModelRegistry();
+            var properties = modelRegistry.Get(type);
+
+            var name2 = properties.GetMapping("Name1");
+            Assert.True(name2 == "name2");
+
+            var nameA = properties.GetMapping("NameA");
+            Assert.True(nameA == "namea");
+
+            var ignore = properties.GetMapping("Ignore");
+            Assert.Null(ignore);
+        }
+    }
+}

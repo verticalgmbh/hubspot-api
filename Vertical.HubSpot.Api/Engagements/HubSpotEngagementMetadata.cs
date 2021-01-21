ec414582f17b500e029cb2512563b83ae0e04626
Axel Kvale <axel@farlig.org>
2021-01-21 13:50:21 +0100

    Extending Engagements with Email engagement type

--- Vertical.HubSpot.Api/Engagements/HubSpotEngagementMetadata.cs
+++ Vertical.HubSpot.Api/Engagements/HubSpotEngagementMetadata.cs
@@ -1,18 +1,16 @@
 using System;
 using System.Collections.Generic;
 using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
 
 namespace Vertical.HubSpot.Api.Engagements
 {
     public class HubspotEngagementMetadata
     {
         [JsonProperty("startTime")]
-        public long StartTime { get; set; }
+        public long? StartTime { get; set; }
 
         [JsonProperty("endTime")]
-        public long EndTime { get; set; }
+        public long? EndTime { get; set; }
 
         [JsonProperty("title")]
         public string Title { get; set; }
@@ -30,9 +28,32 @@ namespace Vertical.HubSpot.Api.Engagements
         public string SourceId { get; set; }
 
         [JsonProperty("createdFromLinkId")]
-        public long CreatedFromLinkId { get; set; }
+        public long? CreatedFromLinkId { get; set; }
 
         [JsonProperty("preMeetingProspectReminders")]
         public List<object> PreMeetingProspectReminders { get; set; }
+
+        [JsonProperty("cc")]
+        public List<HubspotEngangementContact> Cc { get; set; }
+
+        [JsonProperty("bcc")]
+        public List<HubspotEngangementContact> Bcc { get; set; }
+
+        [JsonProperty("subject")]
+        public string Subject { get; set; }
+
+        [JsonProperty("html")]
+        public string Html { get; set; }
+
+        [JsonProperty("text")]
+        public string Text { get; set; }
+
+        [JsonProperty("from")]
+        public HubspotEngangementContact From { get; set; }
+
+        [JsonProperty("to")]
+        public List<HubspotEngangementContact> To { get; set; }
+        [JsonProperty("sender")]
+        public HubspotEngangementContact Sender { get; set; }
     }
 }
---
+++ Vertical.HubSpot.Api/Engagements/HubspotEngangementContact.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vertical.HubSpot.Api.Engagements
+{
+    public class HubspotEngangementContact
+    {
+        [JsonProperty("email")]
+        public string Email { get; set; }
+        [JsonProperty("firstName")]
+        public string FirstName { get; set; }
+        [JsonProperty("lastName")]
+        public string LastName { get; set; }
+        [JsonProperty("raw")]
+        public string Raw { get; set; }
+    }
+}
